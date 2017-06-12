07837e9198cf84400cbe2fc9ed297dc8caf900c6
Yethiel <marvin.thiel@stud.uni-goettingen.de>
2017-06-12 16:13:21 +0200

    Game object cleanup

--- modules/game.py
+++ modules/game.py
@@ -10,12 +10,12 @@ class Game:
     """
     def __init__(self):
         self.__ships = {}    # dictionary of ships (id:ship)
-        self.level = None
-        self.mode = None
-        self.music_dir = None
-        self.ship_possessions = {}
+        self.level = None    # current level object
+        self.mode = None     # current mode (string)
+        self.music_dir = None   # current music directory (string)
+        self.ship_possessions = {}  # dictionary player_id:ship_id
 
-        # lists for game content
+        # lists for game content (strings of folder names)
         self.level_list = []
         self.ship_list = []
         self.mode_list = []
@@ -28,15 +28,18 @@ class Game:
         return ship_id
 
     def get_ship(self, ship_id):
+        """Get the ship object with the id (int)"""
         return self.__ships[ship_id]
 
     def assign_ship_to_player(self, ship_id, player_id):
+        """Assigns ship id to player id"""
         self.ship_possessions[player_id] = ship_id
         if G.DEBUG: print("GAME: Assigned ship {} to player {}".format(
             ship_id, player_id))
         return self.ship_possessions[player_id]
 
     def get_ship_by_player(self, player_id):
+        """Returns ship object assigned to player id"""
         if player_id in self.ship_possessions:
             return self.get_ship((self.ship_possessions[player_id]))
         else:
