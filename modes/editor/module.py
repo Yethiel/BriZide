58f2a06c78ad1846048c2f015e3c0d89227729b4
Marv <yethiel@mailbox.org>
2018-12-28 18:48:52 +0100

    wip: editor modes

--- modes/editor/module.py
+++ modes/editor/module.py
@@ -21,12 +21,22 @@ key_select = controls["editor_select"]
 key_deselect = controls["editor_deselect"]
 key_delete = controls["editor_delete"]
 
+key_rotate = controls["editor_rotate"]
+key_grab = controls["editor_grab"]
+
+# edit modes
+SELECT = 0  # select and remove objects
+GRAB   = 1  # move objects
+ROTATE = 2  # rotate objects
+ADD    = 3  # add objects to the scene
+
 
 class Edit_Mode(Game_Mode):
     def __init__(self, game_obj):
         # initiates the game mode, name needs to match folder name
         super().__init__(required_components, game_obj, 'editor')
         self.selection = {}
+        self.mode = SELECT  # editor starts in select mode
 
     def setup(self):
         """ runs after loading is done """
@@ -42,11 +52,11 @@ class Edit_Mode(Game_Mode):
 
         # creates the UI
         layout = logic.ui['editor']
-        label_selected_block = btk.Label(layout, 
-            text='block',
+        label_mode = btk.Label(layout, 
+            text='Mode:',
             position=[0.4, 0.4, 0],
             size=0.3,
-            update=update_label_selected_block)
+            update=update_label_mode)
 
         """ Workaround:
             Libload loads all scenes from a blend file so the ones
@@ -73,6 +83,8 @@ class Edit_Mode(Game_Mode):
         if scene_ui is None:  # skips if scene isn't loaded yet
             return
         cursor = get_scene('UI_Editor').objects['cursor']
+
+        # synchronizes the cameras
         scene_ui.objects['camera_editor_ui'].worldPosition = camera.worldPosition
         scene_ui.objects['camera_editor_ui'].worldOrientation = camera.worldOrientation
 
@@ -95,45 +107,66 @@ class Edit_Mode(Game_Mode):
                 int(self.old_mouse_pos[1] * winh))
             mouse.visible = True
 
-        # left control modifier
-        if keystat('LEFTCTRLKEY', 'ACTIVE'):
-            blocks = [obj for obj in scene.objects if "Block_" in obj.name]
-            if keystat('AKEY', 'JUST_RELEASED'):
-                if len(self.selection.keys()) != len(blocks):
-                    self.deselect_all()
-                    for obj in blocks:
-                        self.select_multiple(obj)
-                else:
-                    self.deselect_all()
+        # branches out into modes
+        if self.mode == SELECT:
 
-            return  # accepts no other keys
+            # rotation mode on R
+            if keystat(key_rotate, 'JUST_RELEASED'):
+                self.mode = ROTATE
+                return
 
-        # left shift modifier
-        if keystat('LEFTSHIFTKEY', 'ACTIVE'):
-            # add object
-            if keystat('AKEY', 'JUST_RELEASED'):
-                print("Add object")
+            # grab mode on G
+            if keystat(key_grab, 'JUST_RELEASED'):
+                self.mode = GRAB
+                return
 
+            # left shift modifier
+            if keystat('LEFTSHIFTKEY', 'ACTIVE'):
+                hit_obj = sensor_mouse.hitObject
+                # sets the cursor to the selected object
+                if keystat(key_select, 'JUST_ACTIVATED') and hit_obj != None:
+                    cursor.worldPosition = hit_obj.worldPosition
+                    self.select_multiple(hit_obj)
+                # add object
+                if keystat('AKEY', 'JUST_RELEASED'):
+                    self.mode = ADD
+                    return
+                return
+
+            # left control modifier
+            if keystat('LEFTCTRLKEY', 'ACTIVE'):
+                blocks = [obj for obj in scene.objects if "Block_" in obj.name]
+                if keystat('AKEY', 'JUST_RELEASED'):
+                    if len(self.selection.keys()) != len(blocks):
+                        self.deselect_all()
+                        for obj in blocks:
+                            self.select_multiple(obj)
+                    else:
+                        self.deselect_all()
+                return
+
+            # selection
             hit_obj = sensor_mouse.hitObject
-            # sets the cursor to the selected object
-            if keystat(key_select, 'JUST_ACTIVATED') and hit_obj != None:
-                cursor.worldPosition = hit_obj.worldPosition
-                self.select_multiple(hit_obj)
+            if hit_obj != None:
+                # sets the cursor to the selected object
+                if keystat(key_select, 'JUST_ACTIVATED'):
+                    cursor.worldPosition = hit_obj.worldPosition
+                    self.select_single(hit_obj)
 
-            return  # accepts no other keys
+            # delete selection
+            if keystat(key_delete, 'JUST_RELEASED'):
+                self.delete_selection()
 
-        # selection
-        hit_obj = sensor_mouse.hitObject
-        if hit_obj != None:
-            # sets the cursor to the selected object
-            if keystat(key_select, 'JUST_ACTIVATED'):
-                cursor.worldPosition = hit_obj.worldPosition
-                self.select_single(hit_obj)
+        elif self.mode == GRAB:
+            if keystat('BACKSPACEKEY', 'JUST_RELEASED'):
+                self.mode = SELECT
+        elif self.mode == ROTATE:
+            if keystat('BACKSPACEKEY', 'JUST_RELEASED'):
+                self.mode = SELECT
+        elif self.mode == ADD:
+            if keystat('BACKSPACEKEY', 'JUST_RELEASED'):
+                self.mode = SELECT
 
-        # delete selection
-        if keystat(key_delete, 'JUST_RELEASED'):
-            self.delete_selection()
-        
         # camera movement
         if keystat(key_left, 'ACTIVE'):
             camera.applyMovement([-2, 0, 0], True)
@@ -166,6 +199,9 @@ class Edit_Mode(Game_Mode):
             self.selection[o].endObject()
             del self.selection[o]
             o.endObject()
+
+    def get_mode(self):
+        return ['Select', 'Grab', 'Rotate', 'Add'][self.mode]
 
     def select_single(self, obj):
         """ Selects a single object """
@@ -206,5 +242,5 @@ def main():
 
 # UI functions
 
-def update_label_selected_block(widget):
-    widget.text = 'editor'
+def update_label_mode(widget):
+    widget.text = 'Mode: {}'.format(logic.editor.get_mode())
