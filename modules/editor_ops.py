b27dda7ef23502aaed36a095f24ff49e69c5871e
Marv <yethiel@mailbox.org>
2019-01-27 21:39:20 +0100

    remove old editor files

--- modules/editor_ops.py
+++
@@ -1,9 +0,0 @@
-from bge import logic
-
-gD = logic.globalDict
-
-def delete_block(argument=None):
-    if not "undeletable" in gD["editor"]["active_block"]:
-        gD["editor"]["active_block"].endObject()
-    else:
-        if G.DEBUG: print(own.name, "Cannot delete block.")
